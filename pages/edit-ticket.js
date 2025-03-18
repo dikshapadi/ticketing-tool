7caaed5e55328bc96d70cd24a3c9b5f01096d058
dikshapadi <dikshapadi@gmail.com>
2025-03-18 12:57:30 +0530

    added deployment.yaml, secret.yaml

--- pages/edit-ticket.js
+++ pages/edit-ticket.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Navbar from '../../../components/Navbar';
+import Navbar from '../components/Navbar';
 
 const EditTicket = () => {
     const router = useRouter();
